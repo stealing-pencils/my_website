23cce446e42effd7747d40e4c1ced28dfd2a96b4
Katherine Riley <unoscarto@hotmail.com>
2018-12-07 15:52:44 -0800

    base line to start project from

--- my_website/src/App.js
+++ my_website/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App-container">
         <header className="App-header">
           <h1>
           Stealing Pencils under construction
