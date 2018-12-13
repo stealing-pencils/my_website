41777086e112ae1b3cec5dc17d3ba65ab9540cc1
Katherine Riley <unoscarto@hotmail.com>
2018-12-12 16:09:42 -0800

    basic structure for blog page

--- my_website/src/App.js
+++ my_website/src/App.js
@@ -10,6 +10,34 @@ class App extends Component {
           Stealing Pencils under construction
           </h1>
         </header>
+        <body>
+          <div className="title-image">
+          </div>
+          <div className="about-column-right">
+            <h3>
+            About Me
+            </h3>
+          </div>
+          <div className="main-blog-container">
+            <h2>
+            Here is my blog
+            </h2>
+            <div className="posts">
+              <h4>
+                <div className="post-title">
+                </div>
+              </h4>
+              <div className="post-text">
+              </div>
+            </div>
+          </div>
+        </body>
+        <footer>
+          <div className="social-media">
+          </div>
+          <div className="contact-details">
+          </div>
+        </footer>
       </div>
     );
   }
