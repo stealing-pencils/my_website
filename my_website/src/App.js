cd382d77a5fac6403de095204946a5ce7ca16d7c
Katherine Riley <unoscarto@hotmail.com>
2018-12-13 15:29:29 -0800

    added padding to all structural elements

--- my_website/src/App.js
+++ my_website/src/App.js
@@ -5,43 +5,44 @@ class App extends Component {
   render() {
     return (
       <div className="App-container">
-        <div className="header">
-          <header className="App-header">
-            <h1>
-            Stealing Pencils under construction
-            </h1>
-          </header>
-        </div>
-
-        <div className="title-image">
-        </div>
-        <div className="about-column-right">
-          <h3>
-          About Me
-          </h3>
-        </div>
-        <div className="main-blog-container">
-          <div className="blog-title">
-            <h2>
-            Here is my blog
-            </h2>
+        <div className="App-content">
+          <div className="header">
+            <header className="App-header">
+              <h1>
+              Stealing Pencils under construction
+              </h1>
+            </header>
           </div>
-          <div className="posts">
-            <h4 className="post-title">
-              Here is a post title
-            </h4>
-            <div className="post-text">
-            Here is post text
+          <div className="title-image">
+          </div>
+          <div className="about-column-right">
+            <h3>
+            About Me
+            </h3>
+          </div>
+          <div className="main-blog-container">
+            <div className="blog-title">
+              <h2>
+              Here is my blog
+              </h2>
+            </div>
+            <div className="posts">
+              <h4 className="post-title">
+                Here is a post title
+              </h4>
+              <div className="post-text">
+              Here is post text
+              </div>
             </div>
           </div>
+          <footer>
+            This is a footer
+            <div className="social-media">
+            </div>
+            <div className="contact-details">
+            </div>
+          </footer>
         </div>
-        <footer>
-          This is a footer
-          <div className="social-media">
-          </div>
-          <div className="contact-details">
-          </div>
-        </footer>
       </div>
 
     );
