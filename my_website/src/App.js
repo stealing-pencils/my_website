4b520cf454bfc4666c95cd7f5365834ff05c3a95
Katherine Riley <unoscarto@hotmail.com>
2018-12-19 13:00:43 -0800

    changed title heading font and styling

--- my_website/src/App.js
+++ my_website/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <div className="header">
             <header className="App-header">
               <h1>
-              Stealing Pencils
+              {`Stealing \nPencils.`}
               </h1>
             </header>
           </div>
