6ac0fd13beed9ea449d94e78ed82edfc229d40cf
Katherine Riley <unoscarto@hotmail.com>
2019-01-07 15:49:32 -0800

    fixed order of posts so they flow vertically

--- my_website/src/BlogPosts.js
+++ my_website/src/BlogPosts.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 // import './App.css';
 import './blogPosts.css';
 import jan1st2019 from './images/jan1st2019.jpg'
+import jan2nd2019_image from './images/jan2nd2019.jpg'
 
 
 class BlogPosts extends Component {
   render() {
     return (
       <div className="posts">
-        <h4 className="post-title">
+        <h4 className="post-title-first">
           Starting my new life
         </h4>
-        <div className="post-image">
-          <img className="jan_1st_2019_image" src={jan1st2019} width="100%" height="160%" alt="person standing on edge of cliff"/>
+        <div className="post-image-first">
+          <img src={jan1st2019} width="100%" height="160%" alt="person standing on edge of cliff"/>
         </div>
-        <div className="post-text">
-        Here is post text
+        <div className="post-text-first">
+        "hello"
+        </div>
+        <h4 className="post-title-second">
+          post 2
+        </h4>
+        <div className="post-image-second">
+          <img className="jan2nd2019_image" src={jan2nd2019_image} width="100%" height="160%" alt="coffee cup"/>
+        </div>
+        <div className="post-text-second">
+        "hi again"
         </div>
       </div>
     )
